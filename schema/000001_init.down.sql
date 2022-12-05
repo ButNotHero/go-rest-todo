b5e0040a2fe21cf246f172719d9152e1e2f74cc0
butnothero <butnothero@gmail.com>
2022-12-05 13:49:11 +0700

    feat: added sql migrations

---
+++ schema/000001_init.down.sql
@@ -0,0 +1,11 @@
+DROP TABLE list_item;
+
+DROP TABLE todo_item;
+
+DROP TABLE user_list;
+
+DROP TABLE users;
+
+DROP TABLE todo_list;
+
+
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users
+(
+  id            serial       NOT NULL unique,
+  name          varchar(255) NOT NULL,
+  username      varchar(255) NOT NULL unique,
+  password_hash varchar(255) NOT NULL
+);
+
+CREATE TABLE todo_list
+(
+  id          serial       NOT NULL unique,
+  title       varchar(255) NOT NULL,
+  description varchar(255)
+);
+
+CREATE TABLE user_list
+(
+  id      serial                                          NOT NULL unique,
+  user_id int references users (id) on delete cascade     NOT NULL,
+  list_id int references todo_list (id) on delete cascade NOT NULL
+);
+
+CREATE TABLE todo_item
+(
+  id          serial       NOT NULL unique,
+  title       varchar(255) NOT NULL,
+  description varchar(255),
+  done        boolean      NOT NULL default false
+);
+
+CREATE TABLE list_item
+(
+  id      serial                                          NOT NULL unique,
+  item_id int references todo_item (id) on delete cascade NOT NULL,
+  list_id int references todo_list (id) on delete cascade NOT NULL
+);
